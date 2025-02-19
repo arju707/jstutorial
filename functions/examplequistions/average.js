42897e7da577bc2a9864a5e1b60c8ec75f256e7e
Arjun vinod <arjunvinod707@gmail.com>
2025-02-19 11:37:32 +0530

    functions examplequistions

---
+++ functions/examplequistions/average.js
@@ -0,0 +1,22 @@
+function mark(m1,m2,m3){
+
+    let total= m1+m2+m3;
+
+    return average=total/3;
+}
+console.log(mark(30,40,50))
+
+
+
+
+
+//by using arrow function----------------------------------->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+
+let marks=(m1,m2,m3)=>{
+    let total=m1+m2+m3;
+
+    return average=total/mark.length
+}
+console.log(marks(30,40,50,12))
---
+++ functions/examplequistions/calculatefunctions.js
@@ -0,0 +1,19 @@
+// function add(a,b,c){
+//     // console.log(a,b,c)
+//     return a+b+c  ;
+// }
+// console.log(add(1,2,3))
+
+// array=[1,2,3,4,5,6,7]
+// console.log(add("answer is ", 1,2,3 ),add.length)
+
+
+function elements(array){
+
+    if(Array.isArray(array)){
+
+    }
+    return array.length;
+}
+
+console.log(elements([1,2,3,4,5,7,3,4,5,6,7,5,4,3]))
